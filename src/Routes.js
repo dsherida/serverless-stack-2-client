6b83128867f1082314753b4723b26154d21fd7a1
Doug Sheridan <dsherida@asu.edu>
2018-10-07 18:01:57 -0700

    Split app using react-loadable

--- src/Routes.js
+++ src/Routes.js
@@ -1,30 +1,33 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Home from "./containers/Home";
-import Notes from "./containers/Notes";
-import Login from "./containers/Login";
-import Signup from "./containers/Signup";
-import NewNote from "./containers/NewNote";
-import NotFound from "./containers/NotFound";
 import AppliedRoute from "./components/AppliedRoute";
 import AuthenticatedRoute from "./components/AuthenticatedRoute";
 import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
-import Settings from "./containers/Settings";
-import ResetPassword from "./containers/ResetPassword";
-import ChangePassword from "./containers/ChangePassword";
-import ChangeEmail from "./containers/ChangeEmail";
+import Loadable from 'react-loadable';
+import MyLoadingComponent from "./components/MyLoadingComponent";
+
+const AsyncHome = Loadable({loader: () => import("./containers/Home"), loading: MyLoadingComponent});
+const AsyncNotes = Loadable({loader: () => import("./containers/Notes"), loading: MyLoadingComponent});
+const AsyncLogin = Loadable({loader: () => import("./containers/Login"), loading: MyLoadingComponent});
+const AsyncSignup = Loadable({loader: () => import("./containers/Signup"), loading: MyLoadingComponent});
+const AsyncNewNote = Loadable({loader: () => import("./containers/NewNote"), loading: MyLoadingComponent});
+const AsyncNotFound = Loadable({loader: () => import("./containers/NotFound"), loading: MyLoadingComponent});
+const AsyncSettings = Loadable({loader: () => import("./containers/Settings"), loading: MyLoadingComponent});
+const AsyncResetPassword = Loadable({loader: () => import("./containers/ResetPassword"), loading: MyLoadingComponent});
+const AsyncChangePassword = Loadable({loader: () => import("./containers/ChangePassword"), loading: MyLoadingComponent});
+const AsyncChangeEmail = Loadable({loader: () => import("./containers/ChangeEmail"), loading: MyLoadingComponent});
 
 export default ({ childProps }) =>
   <Switch>
-    <AppliedRoute path="/" exact component={Home} props={childProps} />
-    <UnauthenticatedRoute path="/login" exact component={Login} props={childProps} />
-    <UnauthenticatedRoute path="/signup" exact component={Signup} props={childProps} />
-    <UnauthenticatedRoute path="/login/reset" exact component={ResetPassword} props={childProps} />
-    <AuthenticatedRoute path="/settings" exact component={Settings} props={childProps} />
-    <AuthenticatedRoute path="/settings/email" exact component={ChangeEmail} props={childProps} />
-    <AuthenticatedRoute path="/settings/password" exact component={ChangePassword} props={childProps} />
-    <AuthenticatedRoute path="/notes/new" exact component={NewNote} props={childProps} />
-    <AuthenticatedRoute path="/notes/:id" exact component={Notes} props={childProps} />
+    <AppliedRoute path="/" exact component={AsyncHome} props={childProps} />
+    <UnauthenticatedRoute path="/login" exact component={AsyncLogin} props={childProps} />
+    <UnauthenticatedRoute path="/signup" exact component={AsyncSignup} props={childProps} />
+    <UnauthenticatedRoute path="/login/reset" exact component={AsyncResetPassword} props={childProps} />
+    <AuthenticatedRoute path="/settings" exact component={AsyncSettings} props={childProps} />
+    <AuthenticatedRoute path="/settings/email" exact component={AsyncChangeEmail} props={childProps} />
+    <AuthenticatedRoute path="/settings/password" exact component={AsyncChangePassword} props={childProps} />
+    <AuthenticatedRoute path="/notes/new" exact component={AsyncNewNote} props={childProps} />
+    <AuthenticatedRoute path="/notes/:id" exact component={AsyncNotes} props={childProps} />
     { /* Finally, catch all unmatched routes */ }
-    <Route component={NotFound} />
+    <Route component={AsyncNotFound} />
   </Switch>;
